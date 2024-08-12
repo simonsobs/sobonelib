5ff869c47de6600c5a222e6644a6c6af42adfa3b
Demo User <debian@beaglebone.localdomain>
2024-08-12 21:04:01 +0000

    Updated makefiles for automatic start

--- kill_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NUMP=`ps -a | grep -o "run.sh" | wc -l`
-
-while [ $NUMP -ge 1 ]
-do
-	PID=`ps -a | grep -m 1 "run.sh"| awk '{print $1}'`
-	kill -9 $PID
-	NUMP=`ps -a | grep -o "run.sh" | wc -l`
-done
